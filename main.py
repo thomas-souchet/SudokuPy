a5411f7a6983751e32def7a3c2a3f509e11b8af0
Thomas Souchet <thomassouchet22@gmail.com>
2024-03-04 10:50:20 +0100

    Start sudoku grid UI

--- main.py
+++ main.py
@@ -45,6 +45,25 @@ class App(customtkinter.CTk):
         self.time_lbl = customtkinter.CTkLabel(self.sidebar_frame, text="Time : 32.6s")
         self.time_lbl.pack(pady=16)
 
+        self.buildGrid(3)
+
+    def buildGrid(self, size):
+        fs = 400
+        sudoku_frame = customtkinter.CTkFrame(self, width=fs, height=fs, corner_radius=0, fg_color='transparent')
+        sudoku_frame.grid(row=1, column=1, sticky="nsew")
+
+        for i in range(size):
+            for j in range(size):
+                sub_frame = customtkinter.CTkFrame(sudoku_frame, width=200, height=200, corner_radius=0)
+                sub_frame.grid(row=i, column=j, padx=5, pady=5)
+                self.build_sub_grid(sub_frame, size)
+
+    def build_sub_grid(self, sf, size):
+        for i in range(size):
+            for j in range(size):
+                entry = customtkinter.CTkEntry(sf, width=20, height=32, corner_radius=0, font=('Arial', 32), border_width=0)
+                entry.grid(row=i, column=j, sticky="nsew")
+
 
 if __name__ == "__main__":
     app = App()
